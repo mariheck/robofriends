0a2a2b1c6ef8d57d9517d347c01e9920e61d630f
Marine Heckler <marine.heckler@gmail.com>
2020-07-14 17:55:14 +0200

    Update the app to be a PWA.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ robot }) => (
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
         <img
             alt={robot.name}
-            src={`https://robohash.org/${robot.id}?200x200`}
+            src={`https://robohash.org/${robot.id}?size=200x200`}
         />
         <div>
             <h2>{robot.name}</h2>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 const SearchBox = ({ placeholder, searchChange }) => (
     <div className="pa2">
         <input
+            aria-label="Search Robots"
             className="pa3 ba b--green bg-lightest-blue"
             type="search"
             placeholder={placeholder}
